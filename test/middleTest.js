6d5ae7ffa2172e9d3658144ffa84e689c9a0aa2e
Ralenac <t.ralenac@gmail.com>
2021-09-20 16:36:00 -0400

    Refactoring tail and middle with Mocha and Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,12 +1,33 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-//TEST CODE
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([5, 7, 8, 9]), [7, 8]);
+describe("#middle", () => {
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  it("returns 6 for [5,6,7]", () => {
+    assert.deepEqual(middle([5,6,7]), [6]);
+  });
+  it("returns empty Array for empty Array or Array with less or equal to 2 elements", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+  it("returns empty Array for empty Array or Array with less or equal to 2 elements", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+});
+
+//npm test test/middleTest.js
+
+
+// const assertArraysEqual = require('../assertArraysEqual');
+// const middle = require('../middle');
+
+// //TEST CODE
+// assertArraysEqual(middle([1]), []);
+// assertArraysEqual(middle([1, 2]), []);
+// assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+// assertArraysEqual(middle([5, 7, 8, 9]), [7, 8]);
 
 // node test/middleTest.js
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,16 +1,35 @@
-const assertEqual = require ('../assertEqual');
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
 
-//TEST CODE
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it("returns ['Lighthouse', 'Labs'] for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse', 'Labs']);
+  });
+  it("returns [6,7] for [5,6,7]", () => {
+    assert.deepEqual(tail([5,6,7]), [6,7]);
+  });
+  it("returns empty Array for empty Array or Array with one element", () => {
+    assert.deepEqual(tail(['yo yo']), []);
+  });
+});
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+//npm test test/tailTest.js
 
-const result2 = ["yo yo"];
-console.log(tail(result2));
-assertEqual(result2.length,1); 
+
+
+// const assertEqual = require ('../assertEqual');
+// const tail = require('../tail');
+
+// //TEST CODE
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]);
+
+// const result2 = ["yo yo"];
+// console.log(tail(result2));
+// assertEqual(result2.length,1); 
 
 // node test/tailTest.js
