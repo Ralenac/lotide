1aa7f13ee97fb8467179fc0f588e4e995da621dc
Ralenac <t.ralenac@gmail.com>
2021-09-20 16:54:03 -0400

    Exporting Object with all of the utility functions that our Lotide library offers

---
+++ index.js
@@ -0,0 +1,15 @@
+const assertEqual = require('./assertEqual');
+const head   = require('./head');
+const tail   = require('./tail');
+const middle = require('./middle');
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual'); 
+
+module.exports = {
+  assertEqual: assertEqual,
+  head:   head,
+  tail:   tail,
+  middle: middle,
+  eqArrays: eqArrays,
+  assertArraysEqual: assertArraysEqual
+};
