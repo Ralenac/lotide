a1ba602307f9e4247a98242025fa9b42976e45b1
Ralenac <t.ralenac@gmail.com>
2021-09-20 13:56:32 -0400

    Lotide refactoring

--- assertEqual.js
+++ assertEqual.js
@@ -1,16 +1,18 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('✅✅✅ Assertion Passed: ' + [actual] + ' === ' + [expected]);
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log('🛑🛑🛑 Assertion Failed: ' + [actual] + ' !== ' + [expected]);
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 
 };
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("Lighthouse Labs", "Lighthouse Labs" );
-assertEqual(8, 1);
-assertEqual(9, 9);
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual("Lighthouse Labs", "Lighthouse Labs" );
+// assertEqual(8, 1);
+// assertEqual(9, 9);
+
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,18 +1,27 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+// assertEqual was used before refactoring!
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+
+// const assertEqual = require('./assertEqual'); (in this file you don't need assertEqual because you will require this function in headTest.js)
+
+let head = function (array) {
+  if (array.length === 0) {
+    return undefined
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    return array [0]
   }
 };
 
-let head = function (array) {
-  return array[0] ? array[0] : undefined;
-};
+module.exports = head;
 
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5,6,7]), 8);
-assertEqual(head([]), 5);
+//TEST CODE
+// assertEqual(head([5,6,7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([5,6,7]), 8);
+// assertEqual(head([]), 5);
