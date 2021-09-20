6a68cc165b895fafeabe0450da66ef62183850d5
Ralenac <t.ralenac@gmail.com>
2021-09-20 13:57:52 -0400

    Lotide test file

---
+++ test/assertEqualTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Lighthouse Labs" );
+assertEqual(8, 1);
+assertEqual(9, 9);
+
+//use node test/assertEqualTest.js in lotide directory (not in test subdirectory)
