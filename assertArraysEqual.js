7c12c6f9ef160f4e88bfde8f9577d2bff6276f0b
Ralenac <t.ralenac@gmail.com>
2021-09-20 14:51:04 -0400

    Lotide Refactoring

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,13 @@
-const eqArrays = function(actual, expected){
-  if (JSON.stringify(actual) !== JSON.stringify(expected)) {
-    return false;
-  } else {
-    return true;
-  }
-}; 
+// const eqArrays = function(actual, expected){
+//   if (JSON.stringify(actual) !== JSON.stringify(expected)) {
+//     return false;
+//   } else {
+//     return true;
+//   }
+// }; 
+
+const eqArrays = require('./eqArrays'); //pay attention to dots!!!! only one in same directory
+
 const assertArraysEqual = function(array1, array2){
   if (eqArrays(array1, array2)) {
     console.log(`✅✅✅  Assertion Passed: ${array1} === ${array2}`);
@@ -12,4 +15,7 @@ const assertArraysEqual = function(array1, array2){
     console.log(`🛑🛑🛑  Assertion Failed: ${array1} !== ${array2}`);
   }
 };
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+
+module.exports = assertArraysEqual;
+
+// assertArraysEqual([1, 2, 3], [1, 2, 3]);
