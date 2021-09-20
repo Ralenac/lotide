8ee42dc663aa43f0270825c7902d1d2412b3f756
Ralenac <t.ralenac@gmail.com>
2021-09-20 14:05:26 -0400

    Lotide Refactoring

--- tail.js
+++ tail.js
@@ -1,11 +1,12 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
 
 let tail = function(array) {
   let arr = array.slice(1);
@@ -16,14 +17,16 @@ let tail = function(array) {
   }
 };
 
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+//TEST CODE
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
 
-const result2 = ["yo yo"];
-console.log(tail(result2));
-assertEqual(result2.length,1); 
+// const result = tail(["Hello", "Lighthouse", "Labs"]);
+// assertEqual(result, ["Lighthouse", "Labs"]);
+
+// const result2 = ["yo yo"];
+// console.log(tail(result2));
+// assertEqual(result2.length,1); 
