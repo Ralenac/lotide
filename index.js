1df2be9f1be024f4a15719ca1efcb5722e72d6a5
Ralenac <t.ralenac@gmail.com>
2021-09-20 21:33:51 -0400

    Exporting Objects

--- index.js
+++ index.js
@@ -3,13 +3,34 @@ const head   = require('./head');
 const tail   = require('./tail');
 const middle = require('./middle');
 const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('./assertArraysEqual'); 
+const assertArraysEqual = require('./assertArraysEqual');
+const assertObjectsEqual = require('./assertObjectsEqual'); 
+const countLetters = require('./countLetters'); 
+const countOnly = require('./countOnly'); 
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
-  assertEqual: assertEqual,
-  head:   head,
-  tail:   tail,
-  middle: middle,
-  eqArrays: eqArrays,
-  assertArraysEqual: assertArraysEqual
+  assertEqual,
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertArraysEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  letterPositions,
+  map,
+  takeUntil,
+  without,
+  
 };
